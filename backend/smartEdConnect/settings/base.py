426653fcec8c20bfdb91caeec049eb2da8a4ae82
Abiorh001 <abiolaadedayo1993@gmail.com>
2023-09-12 15:59:34 +0100

    backend initial structure

--- backend/smartEdConnect/settings/base.py
+++ backend/smartEdConnect/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-6ts9)#&n6m_(*t++s)w)9+pgu6gc=m&%#(b27o!j^3ongvvg8#"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

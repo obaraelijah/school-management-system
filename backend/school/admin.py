488c65cdb1cd336ce158565f791e330f17975487
Abiorh001 <abiolaadedayo1993@gmail.com>
2023-09-13 21:06:01 +0100

    school module

--- backend/school/admin.py
+++ backend/school/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import School
 
-# Register your models here.
+
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ('school_name', 'school_email', 'school_logo', 'school_phone_number', 'school_address')
+
+
+admin.site.register(School, SchoolAdmin)
--- backend/smartEdConnect/settings/base.py
+++ backend/smartEdConnect/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ""
+# SECRET_KEY = ""
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework',
+    'school.apps.SchoolConfig'
 ]
 
 MIDDLEWARE = [
@@ -73,12 +76,12 @@ WSGI_APPLICATION = "smartEdConnect.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
@@ -121,3 +124,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'

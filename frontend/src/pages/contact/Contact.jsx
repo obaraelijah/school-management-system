6b62d9726e74cd5d39fec14753b64ab6e7afe5fe
Efezino Idisi <enduranceidisi13@gmail.com>
2023-09-15 02:44:51 +0100

    fix: header

---
+++ frontend/src/pages/contact/Contact.jsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <div>
+      <h2>contact us</h2>
+    </div>
+  );
+};
+
+export default Contact;
--- frontend/src/pages/landing_pge/Home.jsx
+++ frontend/src/pages/landing_pge/Home.jsx
@@ -1,6 +1,9 @@
+import Hero from '../../components/headers/users/Hero';
+
 const Home = () => {
   return (
     <div>
+      <Hero />
       <h2>home</h2>
     </div>
   );

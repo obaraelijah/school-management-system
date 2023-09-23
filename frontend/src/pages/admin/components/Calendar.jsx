e93d36e78807c973ba1225a4e84ae81b9bb61aa6
Efezino Idisi <enduranceidisi13@gmail.com>
2023-09-23 13:40:37 +0100

    remove unused lib

--- frontend/src/pages/admin/components/Calendar.jsx
+++
@@ -1,8 +0,0 @@
-import { Calendar as BigCalendar, momentLocalizer } from 'react-big-calendar';
-import moment from 'moment';
-
-const localizer = momentLocalizer(moment);
-
-export default function Calendar(props) {
-  return <BigCalendar {...props} localizer={localizer} />;
-}

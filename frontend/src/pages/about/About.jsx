25a017c528f4ff84341e52b1ae5614e2f4d2e6c8
Sammie2022 <samuelnganganganga26@gmail.com>
2023-09-26 17:40:13 +0300

    contact and about pages

--- frontend/src/pages/about/About.jsx
+++ frontend/src/pages/about/About.jsx
@@ -1,5 +1,34 @@
+import React from 'react';
+import './About.css'; // Import your CSS file
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="about-container">
+      <div className="about-content">
+        <h1>About Our Education Management System</h1>
+        <p>
+          Welcome to our smartEdConnect Education Management System ,where we
+          empower educational institutions with the tools they need to thrive in the
+          digital age. Our mission is to streamline administrative tasks, enhance
+          communication, and provide valuable insights to educators, students, and
+          parents alike.
+        </p>
+        <p>
+          With smartEdConnect, educational institutions can efficiently manage student records,
+          track academic performance, and simplify communication channels. We believe in
+          the transformative power of technology in education and are committed to
+          supporting institutions in their quest for excellence.
+        </p>
+        <button className="learn-more-button">Learn More</button>
+      </div>
+      <div className="about-image">
+        <img
+          src="https://via.placeholder.com/400x300" // Replace with an image related to your EMS
+          alt="Education Management System"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- frontend/src/pages/contact/Contact.jsx
+++ frontend/src/pages/contact/Contact.jsx
@@ -1,7 +1,27 @@
+import React from 'react';
+import './Contact.css'; // Import your CSS file for styling
+
 const Contact = () => {
   return (
-    <div>
-      <h2>contact us</h2>
+    <div className="contact-container">
+      <div className="contact-content">
+        <h1>Contact Us</h1>
+        <p>
+          Have questions or need assistance with our Education Management
+          System? We're here to help!
+        </p>
+        <div className="contact-details">
+          <p>Email: info@schoolmanagementsystem.com</p>
+          <p>Phone: +1 123-456-7890</p>
+          <p>Address: 123 Main Street, City, Country</p>
+        </div>
+      </div>
+      <div className="contact-image">
+        <img
+          src="/images/contact-image.jpg" // Replace with the correct image path
+          alt="Contact Us"
+        />
+      </div>
     </div>
   );
 };

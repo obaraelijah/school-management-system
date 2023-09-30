3fd80d1c4f28d1692e4bcb983ef649a09d3f006e
Sammie2022 <samuelnganganganga26@gmail.com>
2023-09-30 23:51:31 +0300

    updated contact and about page using tailwind

--- frontend/src/pages/about/About.jsx
+++ frontend/src/pages/about/About.jsx
@@ -1,28 +1,33 @@
 import React from 'react';
-import './About.css'; // Import your CSS file
 import logo from '../images/smartEdConnect.logo.jpg';
 
 const About = () => {
   return (
-    <div className="about-container">
-      <div className="about-content">
-        <h1 className="about-heading">About Our School Management System</h1>
-        <p className="about-paragraph">
-          Welcome to our smartEdConnect School Management System, where we empower
-          educational institutions with the tools they need to thrive in the digital age.
-          Our mission is to streamline administrative tasks, enhance communication, and
-          provide valuable insights to educators, students, and parents alike.
-        </p>
-        <p className="about-paragraph">
-          With smartEdConnect, educational institutions can efficiently manage student
-          records, track academic performance, and simplify communication channels. We
-          believe in the transformative power of technology in education and are committed
-          to supporting institutions in their quest for excellence.
-        </p>
-        <button className="learn-more-button">Learn More</button>
-      </div>
-      <div className="about-image">
-        <img src={logo} alt="About Us" />
+    <div className="bg-gray-100 py-12">
+      <div className="container mx-auto flex items-center">
+        <div className="w-1/2">
+          <h1 className="text-4xl font-semibold text-gray-800 mb-4">
+            About Our School Management System
+          </h1>
+          <p className="text-lg text-gray-600 mb-4">
+            Welcome to our smartEdConnect School Management System, where we empower
+            educational institutions with the tools they need to thrive in the digital age.
+            Our mission is to streamline administrative tasks, enhance communication, and
+            provide valuable insights to educators, students, and parents alike.
+          </p>
+          <p className="text-lg text-gray-600 mb-4">
+            With smartEdConnect, educational institutions can efficiently manage student
+            records, track academic performance, and simplify communication channels. We
+            believe in the transformative power of technology in education and are committed
+            to supporting institutions in their quest for excellence.
+          </p>
+          <button className="bg-blue-500 text-white px-6 py-3 rounded-full hover:bg-blue-600">
+            Learn More
+          </button>
+        </div>
+        <div className="w-1/2">
+          <img src={logo} alt="About Us" className="rounded-lg shadow-lg" />
+        </div>
       </div>
     </div>
   );
--- frontend/src/pages/contact/Contact.jsx
+++ frontend/src/pages/contact/Contact.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import './Contact.css'; // Import your CSS file for styling
 
 const Contact = () => {
   return (
-    <div className="contact-container">
-      <div className="contact-content">
-        <h1>Contact Us</h1>
-        <p>
-          Have questions or need assistance with our Education Management
-          System? We're here to help!
-        </p>
-        <div className="contact-details">
-          <p>Email: info@schoolmanagementsystem.com</p>
-          <p>Phone: +1 123-456-7890</p>
-          <p>Address: 123 Main Street, City, Country</p>
+    <div className="bg-gray-100 py-12">
+      <div className="container mx-auto flex items-center">
+        <div className="w-1/2">
+          <div className="contact-content">
+            <h1 className="text-4xl font-semibold text-gray-800 mb-4">Contact Us</h1>
+            <p className="text-lg text-gray-600 mb-4">
+              Have questions or need assistance with our Education Management System? We're here to help!
+            </p>
+            <div className="contact-details">
+              <p className="text-lg text-gray-600 mb-2">Email: info@schoolmanagementsystem.com</p>
+              <p className="text-lg text-gray-600 mb-2">Phone: +1 123-456-7890</p>
+              <p className="text-lg text-gray-600">Address: 123 Main Street, City, Country</p>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="contact-image">
-        <img
-          src="/images/contact-image.jpg" // Replace with the correct image path
-          alt="Contact Us"
-        />
+        <div className="w-1/2">
+          <img
+            src="/images/contact-image.jpg" // Replace with the correct image path
+            alt="Contact Us"
+            className="rounded-lg shadow-lg"
+          />
+        </div>
       </div>
     </div>
   );
